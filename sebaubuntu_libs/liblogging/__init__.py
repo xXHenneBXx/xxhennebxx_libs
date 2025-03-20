c8bffc092fb8751827f850b06d7d71c3ca53027e
xXHenneBXx <59542715+xXHenneBXx@users.noreply.github.com>
2025-03-20 02:47:52 -0400

    added colour schemes using colorama
    added colour schemes to the logging definitions

--- sebaubuntu_libs/liblogging/__init__.py
+++ sebaubuntu_libs/liblogging/__init__.py
@@ -1,18 +1,28 @@
 #
 # Copyright (C) 2022 Sebastiano Barezzi
-#
+# Copyright (C) 2025 xXHenneBXx
 # SPDX-License-Identifier: Apache-2.0
 #
 """Logging utils."""
 
 from logging import basicConfig, INFO, DEBUG
 from logging import debug, info, warning, error, fatal
+from colorama import Fore, Style
 
-LOGD = debug
-LOGI = info
-LOGW = warning
-LOGE = error
-LOGF = fatal
+def LOGD(message):
+	print(f"{Fore.BLUE}[DEBUG] {Style.RESET_ALL}",{message})
+
+def LOGI(message):
+	print(f"{Fore.GREEN}[INFO] {Style.RESET_ALL}",{message})
+
+def LOGW(message):
+	print(f"{Fore.YELLOW}[WARNING] {Style.RESET_ALL}",{message})
+
+def LOGE(message):
+	print(f"{Fore.MAGENTA}[ERROR] {Style.RESET_ALL}",{message})
+
+def LOGF(message):
+	print(f"{Fore.RED}[FATAL] {Style.RESET_ALL}",{message})
 
 def setup_logging(verbose: bool = False):
 	if verbose:
